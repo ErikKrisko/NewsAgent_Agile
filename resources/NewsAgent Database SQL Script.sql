64a42ecf942cf446eba20955b1e28e7f88e9ca37
Ben R <A00276965@student.ait.ie>
2021-02-04 17:10:49 +0000

    Tweaked table names, fixed few small issues. No errors.

--- resources/NewsAgent Database SQL Script.sql
+++ resources/NewsAgent Database SQL Script.sql
@@ -3,177 +3,178 @@ CREATE SCHEMA NewsAgent;
 USE NewsAgent;
 SET AUTOCOMMIT = 0;
 
-/*Customer Table*/
+ /*Customer Table*/
 DROP TABLE IF EXISTS customer;
 CREATE TABLE customer (
-	customerId INTEGER AUTO_INCREMENT,
-	firstName VARCHAR(11) NOT NULL DEFAULT '',
-	lastName VARCHAR(11) NOT NULL DEFAULT '',
-	phoneNo VARCHAR(12) NOT NULL DEFAULT '',
-	PRIMARY KEY (customerId)
+	customer_id INTEGER AUTO_INCREMENT,
+	first_name VARCHAR(20) NOT NULL DEFAULT '',
+	last_name VARCHAR(20) NOT NULL DEFAULT '',
+	phone_no VARCHAR(12) NOT NULL DEFAULT '',
+	PRIMARY KEY (customer_id)
 );
  
  /*Address Table*/
 DROP TABLE IF EXISTS address;
 CREATE TABLE address (
-	addressId INTEGER AUTO_INCREMENT,
-	fullAddress VARCHAR(50) NOT NULL DEFAULT '',
-	areaCode VARCHAR(10) NOT NULL DEFAULT '',
-	eircode VARCHAR(8) NOT NULL DEFAULT '',
-	PRIMARY KEY (addressId)
+	address_id INTEGER AUTO_INCREMENT,
+	full_address VARCHAR(50) NOT NULL DEFAULT '',
+	area_code VARCHAR(10) NOT NULL DEFAULT '',
+	eir_code VARCHAR(8) NOT NULL DEFAULT '',
+	PRIMARY KEY (address_id)
 );
  
  /*Holiday Table*/
 DROP TABLE IF EXISTS holiday;
 CREATE TABLE holiday (
-	holidayId INTEGER AUTO_INCREMENT,
-	startDate DATE NOT NULL,
-	endDate DATE NOT NULL,
-	PRIMARY KEY (holidayId)
+	holiday_id INTEGER AUTO_INCREMENT,
+	start_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	PRIMARY KEY (holiday_id)
 );
  
  /*Delivery Table*/
 DROP TABLE IF EXISTS delivery;
 CREATE TABLE delivery (
-	deliveryId INTEGER AUTO_INCREMENT,
-	date DATE NOT NULL,
-	PRIMARY KEY (deliveryId)
+	delivery_id INTEGER AUTO_INCREMENT,
+	delivery_date DATE NOT NULL,
+	PRIMARY KEY (delivery_id)
 );
-    /*Employee Table*/
+
+ /*Employee Table*/
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-	employeeId INTEGER AUTO_INCREMENT,
-	firstName VARCHAR(11) NOT NULL DEFAULT '',
-	lastName VARCHAR(11) NOT NULL DEFAULT '',
-	PRIMARY KEY (employeeId)
+	employee_id INTEGER AUTO_INCREMENT,
+	first_name VARCHAR(20) NOT NULL DEFAULT '',
+	last_name VARCHAR(20) NOT NULL DEFAULT '',
+	PRIMARY KEY (employee_id)
 );
 
  /*Publication Table*/
 DROP TABLE IF EXISTS publication;
 CREATE TABLE publication (
-	prodId INTEGER AUTO_INCREMENT,
-	name VARCHAR(25) NOT NULL DEFAULT '',
-	type VARCHAR(15) NOT NULL DEFAULT '',
-	price DOUBLE(4,2) NOT NULL DEFAULT,
-	PRIMARY KEY (prodId)
+	prod_id INTEGER AUTO_INCREMENT,
+	prod_name VARCHAR(25) NOT NULL DEFAULT '',
+	prod_type VARCHAR(15) NOT NULL DEFAULT '',
+	prod_price DOUBLE(4,2) NOT NULL,
+	PRIMARY KEY (prod_id)
 );
  
-  /*Invoice Table*/
+ /*Invoice Table*/
 DROP TABLE IF EXISTS invoice;
 CREATE TABLE invoice (
-	invoiceId INTEGER AUTO_INCREMENT,
-	issueDate DATE NOT NULL,
-	PRIMARY KEY (invoiceId)
+	invoice_id INTEGER AUTO_INCREMENT,
+	issue_date DATE NOT NULL,
+	PRIMARY KEY (invoice_id)
 );
  
-   /*Stock Table*/
+ /*Stock Table*/
 DROP TABLE IF EXISTS stock;
 CREATE TABLE stock (
-	stockId INTEGER AUTO_INCREMENT,
-	deliveryDate DATE NOT NULL,
-	PRIMARY KEY (stockId)
-);
-    /*Frequency Table*/
-DROP TABLE IF EXISTS frequency;
-CREATE TABLE frequency (
-	frequencyId INTEGER AUTO_INCREMENT,
-	baseDate DATE NOT NULL,
-	interval VARCHAR(10) NOT NULL DEFAULT '',
-	PRIMARY KEY (frequencyId)
-);
- 
-  /*Is_in Table*/
-DROP TABLE IF EXISTS customer_lives;
-CREATE TABLE customer_lives (
-	customerId INTEGER NOT NULL,
-	addressId INTEGER NOT NULL,
-	FOREIGN KEY (customerId) REFERENCES customer(customerId),
-	FOREIGN KEY (addressId) REFERENCES address(addressId),
-	PRIMARY KEY (customerId,addressId)	
-);
-	
-/*May_have Table*/
-DROP TABLE IF EXISTS customer_holiday;
-CREATE TABLE customer_holiday (
-	customerId INTEGER NOT NULL,
-	holidayId INTEGER NOT NULL,
-	FOREIGN KEY (customerId) REFERENCES customer(customerId),
-	FOREIGN KEY (holidayId) REFERENCES holiday(holidayId),
-	PRIMARY KEY (customerId,holidayId)	
-);
-	
-/*Must_have Table*/
-DROP TABLE IF EXISTS prod_freq;
-CREATE TABLE prod_freq (
-	prodId INTEGER NOT NULL,
-	frequencyId INTEGER NOT NULL,
-	FOREIGN KEY (prodId) REFERENCES publication(prodId),
-	FOREIGN KEY (frequencyId) REFERENCES frequency(frequencyId),
-	PRIMARY KEY (prodId,frequencyId)	
-);
-	
-/*Contains Table*/
-DROP TABLE IF EXISTS prod_for_delivery;
-CREATE TABLE prod_for_delivery (
-	deliveryId INTEGER NOT NULL,
-	prodId INTEGER NOT NULL,
-	FOREIGN KEY (deliveryId) REFERENCES delivery(deliveryId),
-	FOREIGN KEY (prodId) REFERENCES publication(prodId),
-	PRIMARY KEY (deliveryId,prodIdId)	
+	stock_id INTEGER AUTO_INCREMENT,
+	delivery_date DATE NOT NULL,
+	PRIMARY KEY (stock_id)
 );
 
-/*Delivers Table*/
+ /*Frequency Table*/
+DROP TABLE IF EXISTS frequency;
+CREATE TABLE frequency (
+	freq_id INTEGER AUTO_INCREMENT,
+	freq_date DATE NOT NULL,
+	freq_interval VARCHAR(10) NOT NULL DEFAULT '',
+	PRIMARY KEY (freq_id)
+);
+ 
+ /*Address relation Table*/
+DROP TABLE IF EXISTS customer_lives;
+CREATE TABLE customer_lives (
+	customer_id INTEGER NOT NULL,
+	address_id INTEGER NOT NULL,
+	FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+	FOREIGN KEY (address_id) REFERENCES address(address_id),
+	PRIMARY KEY (customer_id,address_id)
+);
+	
+ /*Holidays relation Table*/
+DROP TABLE IF EXISTS customer_holiday;
+CREATE TABLE customer_holiday (
+    customer_id INTEGER NOT NULL,
+	holiday_id INTEGER NOT NULL,
+	FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+	FOREIGN KEY (holiday_id) REFERENCES holiday(holiday_id),
+	PRIMARY KEY (customer_id,holiday_id)
+);
+	
+ /*Frequency relation Table*/
+DROP TABLE IF EXISTS prod_freq;
+CREATE TABLE prod_freq (
+	prod_id INTEGER NOT NULL,
+	freq_id INTEGER NOT NULL,
+	FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
+	FOREIGN KEY (freq_id) REFERENCES frequency(freq_id),
+	PRIMARY KEY (prod_id, freq_id)
+);
+	
+ /*Delivery - Publication relation Table*/
+DROP TABLE IF EXISTS prod_for_delivery;
+CREATE TABLE prod_for_delivery (
+	delivery_id INTEGER NOT NULL,
+	prod_id INTEGER NOT NULL,
+	FOREIGN KEY (delivery_id) REFERENCES delivery(delivery_id),
+	FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
+	PRIMARY KEY (delivery_id, prod_id)
+);
+
+ /*Employee - Delivery relation Table*/
 DROP TABLE IF EXISTS delivers;
 CREATE TABLE delivers (
-	employeeId INTEGER NOT NULL,
-	deliveryId INTEGER NOT NULL,
-	FOREIGN KEY (employeeId) REFERENCES employee(employeeId),
-	FOREIGN KEY (deliveryId) REFERENCES delivery(deliveryId),
-	PRIMARY KEY (employeeId,deliveryId)	
+	employee_id INTEGER NOT NULL,
+	delivery_id INTEGER NOT NULL,
+	FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+	FOREIGN KEY (delivery_id) REFERENCES delivery(delivery_id),
+	PRIMARY KEY (employee_id, delivery_id)
 );
 	
-/*Completed Table*/
+ /*Delivery - Invoice relation Table*/
 DROP TABLE IF EXISTS delivery_status;
 CREATE TABLE delivery_status (
-    completed VARCHAR(1) NOT NULL DEFAULT '',
-	deliveryId INTEGER NOT NULL,
-	FOREIGN KEY (deliveryId) REFERENCES delivery(deliveryId),
-	PRIMARY KEY (deliveryId)	
+    delivery_status BIT,
+	delivery_id INTEGER NOT NULL,
+	invoice_id INTEGER NOT NULL,
+	FOREIGN KEY (delivery_id) REFERENCES delivery(delivery_id),
+    FOREIGN KEY (invoice_id) REFERENCES invoice(invoice_id),
+	PRIMARY KEY (delivery_id, invoice_id)
 );
 	
-/*Issued_for Table*/
+ /*Invoice - Customer relation Table*/
 DROP TABLE IF EXISTS invoice_for;
 CREATE TABLE invoice_for (
-	customerId INTEGER NOT NULL,
-	invoiceId INTEGER NOT NULL,
-	issue_date DATE NOT NULL,
-	FOREIGN KEY (customerId) REFERENCES customer(customerId),
-	FOREIGN KEY (invoiceId) REFERENCES invoice(invoiceId),
-	PRIMARY KEY (customerId,invoiceId)
+	customer_id INTEGER NOT NULL,
+	invoice_id INTEGER NOT NULL,
+	FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+	FOREIGN KEY (invoice_id) REFERENCES invoice(invoice_id),
+	PRIMARY KEY (customer_id, invoice_id)
 );
 	
-/*Subscribes Table*/
-DROP TABLE IF EXISTS cust_subscription;
-CREATE TABLE cust_subscription (
-	customerId INTEGER NOT NULL,
-	prodId INTEGER NOT NULL,
-	count INTEGER NOT NULL,
-	base_date DATE NOT NULL,
-	interval VARCHAR(10) NOT NULL DEFAULT '',
-	FOREIGN KEY (customerId) REFERENCES customer(customerId),
-	FOREIGN KEY (prodId) REFERENCES publication(prodId),
-    PRIMARY KEY (customerId,prodId)
+ /*Customer - publication relation Table*/
+DROP TABLE IF EXISTS subscription;
+CREATE TABLE subscription (
+	customer_id INTEGER NOT NULL,
+	prod_id INTEGER NOT NULL,
+	prod_count INTEGER NOT NULL,
+-- 	frequency_id INTEGER NOT NULL,
+	FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+	FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
+    PRIMARY KEY (customer_id, prod_id)
 );
-	
-/*In_stock Table*/
-DROP TABLE IF EXISTS delivers;
-CREATE TABLE delivers
-(
-    prodId INTEGER NOT NULL,
-    stockId INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    quantity_received INTEGER NOT NULL,
-    FOREIGN KEY (prodId) REFERENCES publication (prodId),
-    FOREIGN KEY (stockId) REFERENCES stock (stockId),
-    PRIMARY KEY (prodId, stockId)
+
+ /*Publication - Stock relation Table*/
+DROP TABLE IF EXISTS stock_received;
+CREATE TABLE stock_received (
+    prod_id INTEGER NOT NULL,
+    stock_id INTEGER NOT NULL,
+    stock_quantity INTEGER NOT NULL,
+    stock_quantity_received INTEGER NOT NULL,
+    FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
+    FOREIGN KEY (stock_id) REFERENCES stock(stock_id),
+    PRIMARY KEY (prod_id, stock_id)
 );
