f062b3c4446939a4cf72f583be40470359e47f59
Erik <a00269494@student.ait.ie>
2021-02-09 18:53:11 +0000

    SQL Script finished according to DDS2

--- resources/NewsAgent_Database.sql
+++ resources/NewsAgent_Database.sql
@@ -10,6 +10,8 @@ CREATE TABLE customer (
 	first_name VARCHAR(20) NOT NULL DEFAULT '',
 	last_name VARCHAR(20) NOT NULL DEFAULT '',
 	phone_no VARCHAR(12) NOT NULL DEFAULT '',
+	address_id INTEGER NOT NULL,
+    FOREIGN KEY (address_id) REFERENCES address(address_id),
 	PRIMARY KEY (customer_id)
 );
  
@@ -29,6 +31,8 @@ CREATE TABLE holiday (
 	holiday_id INTEGER AUTO_INCREMENT,
 	start_date DATE NOT NULL,
 	end_date DATE NOT NULL,
+    customer_id INTEGER NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
 	PRIMARY KEY (holiday_id)
 );
  
@@ -37,6 +41,11 @@ DROP TABLE IF EXISTS delivery;
 CREATE TABLE delivery (
 	delivery_id INTEGER AUTO_INCREMENT,
 	delivery_date DATE NOT NULL,
+	delivery_status BIT NOT NULL,
+    customer_id INTEGER NOT NULL,
+    invoice_id INTEGER NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY (invoice_id) REFERENCES invoice(invoice_id),
 	PRIMARY KEY (delivery_id)
 );
 
@@ -54,8 +63,10 @@ DROP TABLE IF EXISTS publication;
 CREATE TABLE publication (
 	prod_id INTEGER AUTO_INCREMENT,
 	prod_name VARCHAR(25) NOT NULL DEFAULT '',
-	prod_type VARCHAR(15) NOT NULL DEFAULT '',
-	prod_price DOUBLE(4,2) NOT NULL,
+	prod_type VARCHAR(25) NOT NULL DEFAULT '',
+	prod_price DOUBLE 4,2 NOT NULL,
+	freq_id INTEGER NOT NULL,
+    FOREIGN KEY (freq_id) REFERENCES frequency(freq_id),
 	PRIMARY KEY (prod_id)
 );
  
@@ -64,6 +75,10 @@ DROP TABLE IF EXISTS invoice;
 CREATE TABLE invoice (
 	invoice_id INTEGER AUTO_INCREMENT,
 	issue_date DATE NOT NULL,
+    invoice_status BIT NOT NULL,
+    invoice_total DOUBLE 5,2 NOT NULL,
+    customer_id INTEGER NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
 	PRIMARY KEY (invoice_id)
 );
  
@@ -71,7 +86,12 @@ CREATE TABLE invoice (
 DROP TABLE IF EXISTS stock;
 CREATE TABLE stock (
 	stock_id INTEGER AUTO_INCREMENT,
-	delivery_date DATE NOT NULL,
+	stock_date DATE NOT NULL,
+	stock_quantity INTEGER NOT NULL,
+	stock_code VARCHAR(10) NOT NULL,
+    prod_id INTEGER NOT NULL,
+    stock_quantity_received INTEGER NOT NULL,
+    FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
 	PRIMARY KEY (stock_id)
 );
 
@@ -84,97 +104,38 @@ CREATE TABLE frequency (
 	PRIMARY KEY (freq_id)
 );
  
-/*Address relation Table*/
-DROP TABLE IF EXISTS customer_lives;
-CREATE TABLE customer_lives (
-	customer_id INTEGER NOT NULL,
-	address_id INTEGER NOT NULL,
-	FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-	FOREIGN KEY (address_id) REFERENCES address(address_id),
-	PRIMARY KEY (customer_id,address_id)
-);
-	
-/*Holidays relation Table*/
-DROP TABLE IF EXISTS customer_holiday;
-CREATE TABLE customer_holiday (
-    customer_id INTEGER NOT NULL,
-	holiday_id INTEGER NOT NULL,
-	FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-	FOREIGN KEY (holiday_id) REFERENCES holiday(holiday_id),
-	PRIMARY KEY (customer_id,holiday_id)
-);
-	
-/*Frequency relation Table*/
-DROP TABLE IF EXISTS prod_freq;
-CREATE TABLE prod_freq (
-	prod_id INTEGER NOT NULL,
-	freq_id INTEGER NOT NULL,
-	FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
-	FOREIGN KEY (freq_id) REFERENCES frequency(freq_id),
-	PRIMARY KEY (prod_id, freq_id)
-);
-	
-/*Delivery - Publication relation Table*/
+/*Prod_for_delivery relation Table*/
 DROP TABLE IF EXISTS prod_for_delivery;
 CREATE TABLE prod_for_delivery (
-	delivery_id INTEGER NOT NULL,
 	prod_id INTEGER NOT NULL,
-	FOREIGN KEY (delivery_id) REFERENCES delivery(delivery_id),
+	employee_id INTEGER NOT NULL,
 	FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
-	PRIMARY KEY (delivery_id, prod_id)
+	FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
+	PRIMARY KEY (prod_id,employee_id)
 );
-
-/*Employee - Delivery relation Table*/
+	
+/*Delivers relation Table*/
 DROP TABLE IF EXISTS delivers;
 CREATE TABLE delivers (
-	employee_id INTEGER NOT NULL,
-	delivery_id INTEGER NOT NULL,
+    delivery_id INTEGER NOT NULL,
+    employee_id INTEGER NOT NULL,
+	FOREIGN KEY (delivery_id) REFERENCES delivery(delivery_id),
 	FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
-	FOREIGN KEY (delivery_id) REFERENCES delivery(delivery_id),
-	PRIMARY KEY (employee_id, delivery_id)
-);
-	
-/*Delivery - Invoice relation Table*/
-DROP TABLE IF EXISTS delivery_status;
-CREATE TABLE delivery_status (
-    delivery_status BIT,
-	delivery_id INTEGER NOT NULL,
-	invoice_id INTEGER NOT NULL,
-	FOREIGN KEY (delivery_id) REFERENCES delivery(delivery_id),
-    FOREIGN KEY (invoice_id) REFERENCES invoice(invoice_id),
-	PRIMARY KEY (delivery_id, invoice_id)
-);
-	
-/*Invoice - Customer relation Table*/
-DROP TABLE IF EXISTS invoice_for;
-CREATE TABLE invoice_for (
-	customer_id INTEGER NOT NULL,
-	invoice_id INTEGER NOT NULL,
-	FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-	FOREIGN KEY (invoice_id) REFERENCES invoice(invoice_id),
-	PRIMARY KEY (customer_id, invoice_id)
-);
-	
-/*Customer - publication relation Table*/
-DROP TABLE IF EXISTS subscription;
-CREATE TABLE subscription (
-	customer_id INTEGER NOT NULL,
-	prod_id INTEGER NOT NULL,
-	prod_count INTEGER NOT NULL,
--- 	frequency_id INTEGER NOT NULL,
-	FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-	FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
-    PRIMARY KEY (customer_id, prod_id)
+	PRIMARY KEY (delivery_id,employee_id)
 );
 
-/*Publication - Stock relation Table*/
-DROP TABLE IF EXISTS stock_received;
-CREATE TABLE stock_received (
-    prod_id INTEGER NOT NULL,
-    stock_id INTEGER NOT NULL,
-    stock_quantity INTEGER NOT NULL,
-    stock_quantity_received INTEGER NOT NULL,
-    FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
-    FOREIGN KEY (stock_id) REFERENCES stock(stock_id),
-    PRIMARY KEY (prod_id, stock_id)
+/*Holidays relation Table*/
+DROP TABLE IF EXISTS delivers;
+CREATE TABLE delivers (
+      customer_id INTEGER NOT NULL,
+      prod_id INTEGER NOT NULL,
+      count INTEGER NOT NULL,
+      FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+      FOREIGN KEY (prod_id) REFERENCES publication(prod_id),
+      PRIMARY KEY (customer_id,prod_id)
 );
+
+
+	
+
+	
--- src/testClass.java
+++ src/testClass.java
@@ -2,8 +2,8 @@ import java.sql.SQLException;
 
 public class testClass {
     public static void main(String[] args) throws SQLException {
-        JDBC connection = new JDBC();
-        System.out.println(connection.getConnection().getSchema());
+        JDBC connection = new JDBC("root","admin");
+       // System.out.println(connection.getConnection().getSchema());
 //        connection.test();
         connection.ReadScript("NewsAgent_Database.sql");
         connection.close();
