fdcb0facb4cfb614c81ce6cecccbe4cd0160df64
Erik <a00269494@student.ait.ie>
2021-02-17 13:18:56 +0000

    DB_Delivery nearly finished. TO DO - Validator.

--- src/DB_Delivery.java
+++ src/DB_Delivery.java
@@ -1,4 +1,6 @@
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DB_Delivery
 {
@@ -14,19 +16,136 @@ public class DB_Delivery
     private DB_Customer customer;
     private DB_Invoice invoice;
 
-    public enum attributes {
-        delivery_id(1),
-        delivery_date(2),
-        delivery_status(3),
-        customer(4),
-        invoice(5);
-
-        public final int index;
-
-        attributes(int i){this.index = i;}
-    }
 
     public DB_Delivery() { }
+
+    public DB_Delivery(Date delivery_date, boolean delivery_status, DB_Customer customer, DB_Invoice invoice)
+    {
+        this.delivery_date = delivery_date;
+        this.delivery_status = delivery_status;
+        this.customer = customer;
+        this.invoice = invoice;
+    }
+
+    public DB_Delivery(ResultSet rs) throws DB_DeliveryExceptionHandler
+    {
+        try
+        {
+            delivery_id = rs.getInt(att_delivery.delivery_id.column);
+            delivery_date = rs.getDate(att_delivery.delivery_date.column);
+            delivery_status = rs.getBoolean(att_delivery.delivery_status.column);
+        }
+        catch (SQLException e)
+        {
+            throw new DB_DeliveryExceptionHandler(e.getMessage());
+        }
+    }
+
+    /**Validate Attributes*/
+    private String vEntry(att_delivery type, String entry) throws DB_DeliveryExceptionHandler {
+        switch (type) {
+            case delivery_date -> {
+                if ()
+                    return entry;
+                else
+                    throw new DB_DeliveryExceptionHandler("Invalid delivery_date.");
+            }
+            case delivery_status -> {
+                if (entry == 0 || entry == 1)
+                    return entry;
+                else
+                    throw new DB_DeliveryExceptionHandler("Invalid delivery_status.");
+            }
+
+            default -> throw new DB_DeliveryExceptionHandler("Internal error. Unhandled attribute.");
+        }
+    }
+
+    public String get(att_delivery attribute) throws DB_DeliveryExceptionHandler {
+        switch (attribute) {
+            case delivery_id -> { return "" + delivery_id; }
+            case delivery_date -> { return "" + delivery_date; }
+            case delivery_status -> { return "" + delivery_status; }
+            case customer -> { return "" + customer.getCustomer_id(); }
+            case invoice -> { return "" + invoice.getInvoice_id(); }
+            default -> { throw new DB_DeliveryExceptionHandler("Attribute error"); }
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "DB_Delivery{" +
+                "delivery_id=" + delivery_id +
+                ", delivery_date=" + delivery_date +
+                ", delivery_status=" + delivery_status +
+                ", customer=" + customer +
+                ", invoice=" + invoice +
+                '}';
+    }
+
+    //AUTO GENERATED GETTERS AND SETTERS
+
+    public int getDelivery_id() { return delivery_id; }
+    public Date getDelivery_date() { return delivery_date; }
+    public boolean isDelivery_status() { return delivery_status; }
+    public DB_Customer getCustomer() { return customer; }
+    public DB_Invoice getInvoice() { return invoice; }
+    public void setDelivery_date(Date delivery_date) throws DB_DeliveryExceptionHandler { this.delivery_date = vEntry(att_delivery.delivery_date, delivery_date); }
+    public void setDelivery_status(boolean delivery_status) throws DB_DeliveryExceptionHandler { this.delivery_status = vEntry(att_delivery.delivery_status, delivery_status); }
+    public void setCustomer(DB_Customer customer) { this.customer = customer; }
+    public void setInvoice(DB_Invoice invoice) { this.invoice = invoice; }
+}
+
+enum att_delivery {
+    delivery_id(1, "delivery_id"),
+    delivery_date(2, "delivery_date"),
+    delivery_status(3 , "delivery_status"),
+    customer(4, "customer_id"),
+    invoice(5, "invoice_id");
+
+    public final int column;
+    public final String columnName;
+
+    att_delivery(int c, String cName){this.column = c; this.columnName = cName;}
+}
+
+class search_Delivery {
+    private att_delivery attribute;
+    private String term;
+    private boolean strong;
+
+    public search_Delivery(att_delivery attribute, String term, boolean strong) {
+        this.attribute = attribute;
+        this.term = term;
+        this.strong = strong;
+    }
+
+    //  AUTO GENERATED getters and setters
+    public att_delivery getAttribute() { return attribute; }
+    public void setAttribute(att_delivery attribute) { this.attribute = attribute; }
+    public String getTerm() { return term; }
+    public void setTerm(String term) { this.term = term; }
+    public boolean isStrong() { return strong; }
+    public void setStrong(boolean strong) { this.strong = strong; }
+}
+
+
+class DB_DeliveryExceptionHandler extends Exception
+{
+    String message;
+
+    public DB_DeliveryExceptionHandler(String errMessage){
+        message = errMessage;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
+
+
+
 /*
     public void getByID(int id) throws DB_DeliveryExceptionHandler {
         try {
@@ -47,52 +166,3 @@ public class DB_Delivery
         }
     }
 */
-    public void createDelivery()
-    {
-        //INSERT INTO delivery VALUES(null,?,?,?,?);
-
-    }
-
-    @Override
-    public String toString() {
-        return "DB_Delivery{" +
-                "delivery_id=" + delivery_id +
-                ", delivery_date=" + delivery_date +
-                ", delivery_status=" + delivery_status +
-                ", customer=" + customer +
-                ", invoice=" + invoice +
-                '}';
-    }
-
-    //AUTO GENERATED GETTERS AND SETTERS
-    public int getDelivery_id() { return delivery_id; }
-    public Date getDelivery_date() { return delivery_date; }
-    public void setDelivery_date(Date delivery_date) { this.delivery_date = delivery_date; }
-    public boolean isDelivery_status() { return delivery_status; }
-    public void setDelivery_status(boolean delivery_status) { this.delivery_status = delivery_status; }
-    public DB_Customer getCustomer() { return customer; }
-    public void setCustomer(DB_Customer customer) { this.customer = customer; }
-    public DB_Invoice getInvoice() { return invoice; }
-    public void setInvoice(DB_Invoice invoice) {this.invoice = invoice; }
-
-    //JDBC and Handler Connection
-
-    public static JDBC getConnection() { return connection; }
-    public static void setConnection(JDBC connection) { DB_Delivery.connection = connection; }
-    public static DAO getHandler() { return handler; }
-    public static void setHandler(DAO handler) { DB_Delivery.handler = handler; }
-}
-
-
-class DB_DeliveryExceptionHandler extends Exception
-{
-    String message;
-
-    public DB_DeliveryExceptionHandler(String errMessage){
-        message = errMessage;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-}
