503491d6b0d0584e96c95e4c3733144f8e228a0f
A00271834 <A00271834@student.ait.ie>
2021-02-17 12:54:16 +0000

    Db_invoice alot worked on has alot of errors will fix later

--- src/DB_Invoice.java
+++ src/DB_Invoice.java
@@ -10,8 +10,7 @@ public class DB_Invoice
     private double invoice_total;
     private DB_Customer customer;
 
-    public DB_Invoice(){
-    }
+    public DB_Invoice() throws DB_CustomerExceptionHandler, DB_InvoiceExceptionHandler {
 /*
     public void getByID(JDBC con, int id) throws DB_InvoiceExceptionHandler {
         try {
@@ -80,6 +79,50 @@ s       invoice_id = '"+edit+"'" );
         }
 
     }*/
+    public DB_Invoice(String issue_date, Boolean invoice_status, String invoice_total, DB_Customer customer) throws DB_InvoiceExceptionHandler {
+            this.issue_date = vEntry( Att_Invoice.issue_date, issue_date);
+            this.invoice_status = vEntry( Att_Invoice.invoice_status, invoice_status);
+            this.invoice_total = vEntry( Att_Invoice.invoice_total, invoice_total);
+            this.customer = customer;
+        }
+
+    public DB_Invoice(ResultSet rs) throws DB_InvoiceExceptionHandler {
+            try {
+                invoice_id = rs.getInt( Att_Invoice.invoice_id.column);
+                issue_date = rs.getString( Att_Invoice.issue_date.column);
+                invoice_status = rs.getString( Att_Invoice.invoice_status.column);
+                invoice_total = rs.getString( Att_Invoice.invoice_total.column);
+            } catch (SQLException e) {
+                throw new DB_CustomerExceptionHandler(e.getMessage());
+            }
+        }
+
+        //  Validate attributes
+        private String vEntry(Att_Invoice type, String entry) throws DB_InvoiceExceptionHandler {
+            switch (type) {
+                case issue_date -> {
+                    if (entry.length() > 0 && entry.length() <= 20)
+                        return entry;
+                    else
+                        throw new DB_InvoiceExceptionHandler("Invalid issue date.");
+                }
+                case issue_status -> {
+                    if (entry.length() > 0 && entry.length() <= 20)
+                        return entry;
+                    else
+                        throw new DB_InvoiceExceptionHandler("Invalid issue status.");
+                }
+                case issue_total -> {
+                    if (entry.length() == 10)
+                        return entry;
+                    else
+                        throw new DB_InvoiceExceptionHandler("Invalid issue total.");
+                }
+                default -> throw new DB_CustomerExceptionHandler("Internal error. Unhandled attribute.");
+            }
+        }
+
+    }
 
     @Override
     public String toString() {
@@ -101,6 +144,45 @@ s       invoice_id = '"+edit+"'" );
     public void setInvoice_total(double invoice_total) { this.invoice_total = invoice_total; }
     public DB_Customer getCustomer() { return customer; }
     public void setCustomer(DB_Customer customer) { this.customer = customer; }
+}
+/** List of customer attributes */
+enum Att_Invoice {
+    //  Customer table attributes
+    invoice_id(1, "invoice_id"),
+    issue_date(2, "issue_date"),
+    invoice_status(3, "invoice_status"),
+    invoice_total(4, "invoice_total"),
+    customer(5, "customer");
+
+    //  Column in which the given attribute appears by default
+    public final int column;
+    //  Name of column in which te attribute appears by default
+    public final String name;
+
+    Att_Invoice(int column, String columnName) {
+        this.column = column;
+        this.name = columnName;
+    }
+}
+/** Search object for Invoice WIP! */
+class Search_Invoice {
+    private Att_Invoice attribute;
+    private String term;
+    private boolean strong;
+
+    public Search_Invoice(Att_Invoice  attribute, String term, boolean strong) {
+        this.attribute = attribute;
+        this.term = term;
+        this.strong = strong;
+    }
+
+    //  AUTO GENERATED getters and setters
+    public Att_Invoice  getAttribute() { return attribute; }
+    public void setAttribute(Att_Invoice  attribute) { this.attribute = attribute; }
+    public String getTerm() { return term; }
+    public void setTerm(String term) { this.term = term; }
+    public boolean isStrong() { return strong; }
+    public void setStrong(boolean strong) { this.strong = strong; }
 }
 class DB_InvoiceExceptionHandler extends Exception {
     String message;
