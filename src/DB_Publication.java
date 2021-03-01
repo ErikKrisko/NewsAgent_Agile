da374340d5d62545157a386dc005542d38610840
A00270465 <a00270465@student.ait.ie>
2021-03-01 12:51:10 +0000

    Commented some lines of code also put in comments on what needs to be done or changed

--- src/DB_Publication.java
+++ src/DB_Publication.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -8,25 +7,26 @@ public class DB_Publication {
     private String prod_name, prod_type, frequency;
     private Double prod_price;
     //  External customer attributes
-    private DB_Frequency Frequency;
+    //Leave line 12 commented out for now until DB_Frequency is done. Donny
+    //private DB_Frequency Frequency;
 
     /** Blank constructor */
     public DB_Publication() { }
 
-    public DB_Publication(String prod_name, String type, Double prod_price) throws DB_PublicationExceptionHandler {
-        this.prod_name = vEntry( Att_Publicaton.prod_name, prod_name);
-        this.prod_type = vEntry( Att_Publicaton.prod_type, prod_type);
-        this.prod_price = vEntry( Att_Publicaton.prod_price, prod_price);
-        this.frequency = vEntry( Att_Publicaton.frequency, frequency);
+    public DB_Publication(String prod_name, String type, Double prod_price) throws DB_PublicationExceptionHandler/*,DB_CustomerExceptionHandler probably won't need this expectionHandler Donny*/ {
+        this.prod_name = vEntry( Att_Publication.prod_name, prod_name);
+        this.prod_type = vEntry( Att_Publication.prod_type, prod_type);
+        this.prod_price = vEntry( Att_Publication.prod_price, prod_price); //May need need to change from a string to a double currency is always double Donny
+        this.frequency = vEntry( Att_Publication.frequency, frequency);
     }
 
     public DB_Publication(ResultSet rs) throws DB_PublicationExceptionHandler {
         try {
-            prod_id = rs.getInt( Att_Publicaton.prod_id.column);
-            prod_name = rs.getString( Att_Publicaton.prod_name.column);
-            prod_type = rs.getString( Att_Publicaton.prod_type.column);
-            prod_price = rs.getDouble( Att_Publicaton.prod_price.column);
-            frequency = rs.getString( Att_Publicaton.frequency.column);
+            prod_id = rs.getInt( Att_Publication.prod_id.column);
+            prod_name = rs.getString( Att_Publication.prod_name.column);
+            prod_type = rs.getString( Att_Publication.prod_type.column);
+            prod_price = rs.getDouble( Att_Publication.prod_price.column);
+            frequency = rs.getString( Att_Publication.frequency.column);
         }
         catch (SQLException e) {
             throw new DB_PublicationExceptionHandler(e.getMessage());
@@ -34,7 +34,7 @@ public class DB_Publication {
     }
 
     //  Validate attributes
-    private String vEntry(Att_Publicaton type, String entry) throws DB_CustomerExceptionHandler {
+    private String vEntry(Att_Publication type, String entry) throws DB_CustomerExceptionHandler {
         switch (type) {
             case prod_name -> {
                 if (entry.length() > 0 && entry.length() <= 25)
@@ -64,12 +64,12 @@ public class DB_Publication {
         }
     }
 
-    /** Returns specified Att_Publicaton attribute as a String
+    /** Returns specified Att_Publication attribute as a String
      * @param attribute attribute type to be returned
      * @return an attribute value
      * @throws DB_CustomerExceptionHandler
      */
-    public String get(Att_Publicaton attribute) throws DB_CustomerExceptionHandler {
+    public String get(Att_Publication attribute) throws DB_CustomerExceptionHandler {
         switch (attribute) {
             case prod_id -> { return "" + prod_id; }
             case prod_name -> { return prod_name; }
@@ -98,28 +98,28 @@ public class DB_Publication {
     public Double getprod_price() {   return prod_price; }
     public String getfrequency() {    return frequency; }
     public void setprod_id(long prod_id) { this.prod_id = prod_id; }
-    public void setprod_name(String prod_name) throws DB_CustomerExceptionHandler {  this.prod_name = vEntry( Att_Publicaton.prod_name, prod_name); }
-    public void setprod_type(String prod_type) throws DB_CustomerExceptionHandler {    this.prod_type = vEntry( Att_Publicaton.prod_type, prod_type); }
-    public void setprod_price(String prod_price) throws DB_CustomerExceptionHandler {  this.prod_price = vEntry( Att_Publicaton.prod_price, prod_price); }
+    public void setprod_name(String prod_name) throws DB_CustomerExceptionHandler {  this.prod_name = vEntry( Att_Publication.prod_name, prod_name); }
+    public void setprod_type(String prod_type) throws DB_CustomerExceptionHandler {    this.prod_type = vEntry( Att_Publication.prod_type, prod_type); }
+    public void setprod_price(String prod_price) throws DB_CustomerExceptionHandler {  this.prod_price = vEntry( Att_Publication.prod_price, prod_price); }
 }
 
 
 
 /** Search object for customer WIP! */
 class Search_Publication {
-    private Att_Publicaton attribute;
+    private Att_Publication attribute;
     private String term;
     private boolean strong;
 
-    public Search_Publication(Att_Publicaton attribute, String term, boolean strong) {
+    public Search_Publication(Att_Publication attribute, String term, boolean strong) {
         this.attribute = attribute;
         this.term = term;
         this.strong = strong;
     }
 
     //  AUTO GENERATED getters and setters
-    public Att_Publicaton getAttribute() { return attribute; }
-    public void setAttribute(Att_Publicaton attribute) { this.attribute = attribute; }
+    public Att_Publication getAttribute() { return attribute; }
+    public void setAttribute(Att_Publication attribute) { this.attribute = attribute; }
     public String getTerm() { return term; }
     public void setTerm(String term) { this.term = term; }
     public boolean isStrong() { return strong; }
@@ -135,7 +135,7 @@ class DB_PublicationExceptionHandler extends Exception {
     public String getMessage() {    return message; }
 }
 /** List of customer attributes */
-enum Att_Publicaton {
+enum Att_Publication {
     //  Customer table attributes
     prod_id(1, "prod_id"),
     prod_name(2, "prod_name"),
@@ -145,10 +145,10 @@ enum Att_Publicaton {
 
     //  Column in which the given attribute appears by default
     public final int column;
-    //  prod_name of column in which te attribute appears by default
+    //  prod_name of column in which the attribute appears by default
     public final String column_name;
 
-    Att_Publicaton(int column, String column_name) {
+    Att_Publication(int column, String column_name) {
         this.column = column;
         this.column_name = column_name;
     }
