f5c9a7c4d04d9d2f3b67ac5f679a2a1605062cc1
Conor <obrienconor01@gmail.com>
2021-03-16 17:31:37 +0000

    Publication Tests Version 4

--- test/DB_PublicationTest.java
+++ test/DB_PublicationTest.java
@@ -1,9 +1,19 @@
 import junit.framework.TestCase;
 
+import java.util.Map;
+
 public class DB_PublicationTest extends TestCase {
     DB_Publication publication = new DB_Publication();
 
-    //Test 1 - test for too long of an input//
+    /**
+     * TEST 001
+     * Test name for a string that is longer than the character limit
+     * ============================
+     * Inputs: String
+     * TheSundayMailMirrorExpressTimes
+     * ============================
+     * Expected Outputs: Name = TheSundayMailMirrorExpressTimes is too long
+     */
     public void testDB_Publication001()
     {
         String entry = "TheSundayMailMirrorExpressTimes";
@@ -17,7 +27,15 @@ public class DB_PublicationTest extends TestCase {
         }
     }
 
-    //Test 2 - input of an empty string//
+    /**
+     * TEST 002
+     * Test Name for a string that is empty
+     * ============================
+     * Inputs: String
+     * " "
+     * ============================
+     * Expected Outputs: Name = cannot be Empty
+     */
     public void testDB_publication002()
     {
         String entry = "";
@@ -30,17 +48,36 @@ public class DB_PublicationTest extends TestCase {
 
         }
     }
-    //test 3 - a correct input within the parameters for product name//
+
+    /**
+     * TEST 003
+     * Test Name for a string that meets parameters & passes
+     * ============================
+     * Inputs: String
+     * "Westmeath Examiner"
+     * ============================
+     * Expected Outputs: Name = "Westmeath Examiner"
+     */
     public void testDB_Publication003()
     {
         try {
             publication.validateName("Westmeath Examiner");
+            fail("Exception not expected.");
         } catch (DB_PublicationExceptionHandler e)
         {
-            fail("Exception not expected.");
+            assertEquals("Westmeath Examiner", e.getMessage());
         }
     }
-    //Test 4 - Fail to enter a correct parameter for product type//
+
+    /**
+     * TEST 004
+     * Test type for an invalid type
+     * ============================
+     * Inputs: String
+     * "Paper"
+     * ============================
+     * Expected Outputs: Name = "Type = Paper, must be either Tabloid, Broadsheet, or Magazine"
+     */
     public void testDB_Publication004()
     {
         try {
@@ -48,10 +85,19 @@ public class DB_PublicationTest extends TestCase {
             fail("Exception Expected.");
         } catch (DB_PublicationExceptionHandler e)
         {
-            assertEquals("must be either Tabloid, Broadsheet, or Magazine", e.getMessage());
+            assertEquals("Type = Paper, must be either Tabloid, Broadsheet, or Magazine", e.getMessage());
         }
     }
-    //Test 5 - enter correct parameter for Type//
+
+    /**
+     * TEST 005
+     * Test type for an valid type
+     * ============================
+     * Inputs: String
+     * "Tabloid"
+     * ============================
+     * Expected Outputs: Name = "Type = Tabloid"
+     */
     public void testDB_Publication005()
     {
         try {
@@ -59,38 +105,67 @@ public class DB_PublicationTest extends TestCase {
         } catch (DB_PublicationExceptionHandler e)
         {
             fail("Exception not expected");
+            assertEquals("Type = Tabloid", e.getMessage());
         }
     }
-    //Test 6 - entering 0 as a value for price//
+    /**
+     * TEST 006
+     * Test Price for an entry of zero
+     * ============================
+     * Inputs: double
+     * 0.00
+     * ============================
+     * Expected Outputs: Name = "Price = " 0.00 ", must be greater than zero and less than ten."
+     */
     public void testDB_Publication006()
     {
         try {
             publication.validatePrice(0.00);
-            fail("Exception Expected.");
         } catch (DB_PublicationExceptionHandler e)
         {
+            fail("Exception Expected.");
             assertEquals("Price = must be greater than zero and less than ten.",e.getMessage());
         }
     }
-    //test 7 - entering a value greater than 10//
+
+    /**
+     * TEST 007
+     * Test Price for an entry greater than ten
+     * ============================
+     * Inputs: double
+     * 12.00
+     * ============================
+     * Expected Outputs: Name = "Price = " 12.00 ", must be greater than zero and less than ten."
+     */
     public void testDB_Publication007()
     {
         try {
             publication.validatePrice(12.00);
-            fail("Exception Expected.");
+
         } catch (DB_PublicationExceptionHandler e)
         {
+            fail("Exception Expected.");
             assertEquals("Price = must be greater than zero and less than ten.",e.getMessage());
         }
     }
-    //test 8 - Entering a value within the parameters //
+    /**
+     * TEST 008
+     * Test Price for an entry of zero
+     * ============================
+     * Inputs: double
+     * 5.00
+     * ============================
+     * Expected Outputs: Name = "Price = " 5.00 ",
+     */
     public void testDB_Publication008()
     {
+        Double Entry = 5.00;
         try {
-            publication.validatePrice(5.00);
+            publication.validatePrice(Entry);
         } catch (DB_PublicationExceptionHandler e)
         {
             fail("Exception Not Expected.");
+            assertEquals(Price = + Entry +);
         }
     }
     //Test 9 - entering a Frequency Value greater 25 //
