756a43e1f86fcb168e4e697e1e21beafc84258f9
A00271834 <A00271834@student.ait.ie>
2021-04-13 19:31:56 +0100

    Test for erros

--- src/Tab.java
+++ src/Tab.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 
 public class Tab {
     private final JTabbedPane pane;
-    private final JButton swap_customer = new JButton("Customer"), swap_invoice = new JButton("Invoice");
+    private JButton blank_customer;
+    private JButton blank_invoice;
     private JPanel component;
     private final DAO dao;
 
@@ -25,37 +26,30 @@ public class Tab {
         return component = new customerTab();
     }
 
-    public JPanel invoice() {
-        return component = new invoiceTab();
-    }
-
-//    public JPanel delivery(){
-//        return component = new deliveryTab();
+//    public JPanel blank1(){
+//        return component = new blankTab1();
+//    }
+//
+//    public JPanel invoice() {
+//        return component = new invoiceTab();
 //    }
 
     //  Blank Tab
     private class blankTab extends JPanel implements ActionListener{
         private blankTab() {
             setLayout(new FlowLayout());
-            //  Change to customer
-            swap_customer.addActionListener(this);
-            add(swap_customer);
-            //  Change to invoice
-            swap_invoice.addActionListener(this);
-            add(swap_invoice);
+            blank_customer = new JButton("Customer");
+            blank_customer.addActionListener(this);
+            add(blank_customer);
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
             //  Replace tab with new Tab
-            if (e.getSource() == swap_customer) {
+            if (e.getSource() == blank_customer) {
                 int pos = pane.indexOfComponent(component);
                 pane.setComponentAt(pos, new customerTab());
                 pane.setTitleAt(pos, "Customer");
-            } else if (e.getSource() == swap_invoice) {
-                int pos = pane.indexOfComponent(component);
-                pane.setComponentAt(pos, new invoiceTab());
-                pane.setTitleAt(pos, "Invoice");
             }
         }
     }
@@ -129,82 +123,96 @@ public class Tab {
                 } catch (DAOExceptionHandler exc) {
                     exc.printStackTrace();
                 }
-            }
-        }
-    }
-
-    //  ========================================================================================================================
-    //  INVOICE TAB
-    //  ========================================================================================================================
-    private class invoiceTab extends JPanel implements ActionListener {
-        private final JButton button_search = new JButton("Search");
-        //  Top panel to put search functionality into
-        private final JPanel searchPanel = new JPanel();
-        //  ScrollPane to be used by JTable
-        private final JScrollPane invoice_tablePane = new JScrollPane();
-        //  JTable and TableModel for it
-        private final JTable invoice_table = new JTable() {
-            //  Disable direct editing of the table will need to implement it separately
-            public boolean isCellEditable(int row, int column) {
-                return false;
-            }
-        };
-        private DefaultTableModel invoice_tableModel;
-        //  ArrayList for customers
-        private ArrayList<DB_Invoice> invoice;
-
-        //  Constructor WIP
-        private invoiceTab() {
-            //  Set layout
-            setLayout(new BorderLayout());
-            //  Add both panes
-            add(searchPanel, BorderLayout.NORTH);
-            add(invoice_tablePane, BorderLayout.CENTER);
-            //  Search pane
-            searchPanel.add(button_search);
-            button_search.addActionListener(this);
-            //  Table pane
-            invoice_tablePane.getViewport().add(invoice_table);
-            buildTableModel();
-        }
-
-        //  Builds the table headers (columns)
-        private void buildTableModel() {
-            //  Table model
-            invoice_tableModel = new DefaultTableModel();
-            //  Do the headers
-            invoice_tableModel.addColumn("ID");
-            invoice_tableModel.addColumn("Issue Date");
-            invoice_tableModel.addColumn("Invoice Status");
-            invoice_tableModel.addColumn("Invoice Total");
-            invoice_tableModel.addColumn("Customer");
-            //  set table to use the model
-            invoice_table.setModel(invoice_tableModel);
-            //  disable moving columns around
-            invoice_table.getTableHeader().setReorderingAllowed(false);
-        }
-
-        //  Populates data from customers ArrayList
-        private void updateTableModel() {
-            for (DB_Invoice inv : invoice) {
-//                invoice_tableModel.addRow(inv.getRowData());
-            }
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            //  If search button is pressed
-            //  MAKE ACTUAL INVOICE SEARCH USING INVOICE METHODS that are developed.
-            if (e.getSource() == button_search) {
-//                try {
-//                    //  Get new data (no search criteria for now)
-//                    invoice = dao.getInvoice(new Search_Invoice()[0]);
-//                    //  Update table
-//                    updateTableModel();
-//                } catch (DAOExceptionHandler exc) {
-//                    exc.printStackTrace();
+//                //Invoice
+//                //  Blank Tab
+//                private class blankTab1 extends JPanel implements ActionListener {
+//                    private blankTab1() {
+//                        setLayout(new FlowLayout());
+//                        blank_invoice = new JButton("Invoice");
+//                        blank_invoice.addActionListener(this);
+//                        add(blank_invoice);
+//                    }
+//
+                //Hello
+//                    @Override
+//                    public void actionPerformed(ActionEvent e) {
+//                        //  Replace tab with new Tab
+//                        if (e.getSource() == blank_invoice) {
+//                            int pos = pane.indexOfComponent(component);
+//                            pane.setComponentAt(pos, new invoiceTab());
+//                            pane.setTitleAt(pos, "Invoice");
+//                        }
+//                    }
 //                }
+//
+//                class invoiceTab extends JPanel implements ActionListener {
+//                    private final JButton button_search = new JButton("Search");
+//                    //  Top panel to put search functionality into
+//                    private final JPanel searchPanel = new JPanel();
+//                    //  ScrollPane to be used by JTable
+//                    private final JScrollPane invoice_tablePane = new JScrollPane();
+//                    //  JTable and TableModel for it
+//                    private final JTable invoice_table = new JTable() {
+//                        //  Disable direct editing of the table will need to implement it separately
+//                        public boolean isCellEditable(int row, int column) {
+//                            return false;
+//                        }
+//                    };
+//                    private DefaultTableModel customer_tableModel;
+//                    //  ArrayList for customers
+//                    private ArrayList<DB_Invoice> invoice;
+//
+//                    //  Constructor WIP
+//                    private invoiceTab() {
+//                        //  Set layout
+//                        setLayout(new BorderLayout());
+//                        //  Add both panes
+//                        add(searchPanel, BorderLayout.NORTH);
+//                        add(customer_tablePane, BorderLayout.CENTER);
+//                        //  Search pane
+//                        searchPanel.add(button_search);
+//                        button_search.addActionListener(this);
+//                        //  Table pane
+//                        customer_tablePane.getViewport().add(invoice_table);
+//                        buildTableModel();
+//                    }
+//
+//                    //  Builds the table headers (columns)
+//                    private void buildTableModel() {
+//                        //  Table model
+//                        DefaultTableModel invoice_tableModel = new DefaultTableModel();
+//                        //  Do the headers
+//                        invoice_tableModel.addColumn("ID");
+//                        invoice_tableModel.addColumn("Issue Date");
+//                        invoice_tableModel.addColumn("Invoice Status");
+//                        invoice_tableModel.addColumn("Invoice Total");
+//                        invoice_tableModel.addColumn("Customer");
+//                        //  set table to use the model
+//                        invoice_table.setModel(invoice_tableModel);
+//                        //  disable moving columns around
+//                       invoice_table.getTableHeader().setReorderingAllowed(false);
+//                    }
+//
+//                    //  Populates data from customers ArrayList
+//                    private void updateTableModel() {
+//                        for (DB_Invoice inv : invoice) {
+//                            invoice_tableModel.addRow(inv.getRowData());
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void actionPerformed(ActionEvent e) {
+//                        //  If search button is pressed
+//                        if (e.getSource() == button_search) {
+//                            try {
+//                                //  Get new data (no search criteria for now)
+//                                invoice = dao.getInvoice(new Search_Invoice()[0]);
+//                                //  Update table
+//                                updateTableModel();
+//                            } catch (DAOExceptionHandler exc) {
+//                                exc.printStackTrace();
+//                            }
+                        }
+                    }
+                }
             }
-        }
-    }
-}
