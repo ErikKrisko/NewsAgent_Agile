396beef8bc7a52b8ff7fb2bbac5db14a9e82541f
Ben R <A00276965@student.ait.ie>
2021-04-14 15:23:58 +0100

    Small changes to the TAB section of customer. Huge implementation of Editor JDialog. Similar concept to the one used in Tabs but somewhat different. The link between text field and actual object not yet implemented but its a small addition to make at this stage.

---
+++ src/Editor.java
@@ -0,0 +1,115 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Editor {
+    private final DAO dao;
+
+    public Editor(DAO dao) {
+        this.dao = dao;
+    }
+
+    //  Specific panel constructor
+    public JDialog customer(DB_Customer customer, JFrame parent) { return new customerEdit(customer, parent); }
+
+    private class customerEdit extends JDialog implements ActionListener {
+        private DB_Customer customer;
+        //  TextFields
+        private final JTextField fName = new JTextField(10),
+                lName = new JTextField(10),
+                phoneNo = new JTextField(10),
+                addressID = new JTextField(2);
+        //  Buttons
+        private final JButton bUpdate = new JButton("Update"),
+                bDelete = new JButton("Delete"),
+                bCancel = new JButton("Cancel");
+
+        //  Constructor
+        private customerEdit(DB_Customer customer, JFrame parentFrame) {
+            this.customer = customer;
+            //  Disable main window when this is overplayed
+            parentFrame.setEnabled(false);
+            //  Add action listener to re-enable after closing
+            this.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    parentFrame.setEnabled(true);
+                    super.windowClosed(e);
+                    dispose();
+                }
+            });
+
+            //  set border layout
+            getContentPane().setLayout(new BorderLayout());
+            //  add buttons to SOUTH
+            JPanel buttonBar = new JPanel(new FlowLayout());
+            buttonBar.add(bUpdate);
+            bUpdate.addActionListener(this);
+            buttonBar.add(bDelete);
+            bDelete.addActionListener(this);
+            buttonBar.add(bCancel);
+            bCancel.addActionListener(this);
+            getContentPane().add(buttonBar, BorderLayout.SOUTH);
+            //  Add main panel
+            JPanel view = new JPanel(new FlowLayout());
+            getContentPane().add(view, BorderLayout.CENTER);
+            //  populate view
+            if (customer.getCustomer_id() == 0) {
+                view.add(new JLabel("ID: "));
+                JTextField idField = new JTextField(2);
+                idField.setText("-");
+                idField.setEditable(false);
+                view.add(idField);
+                setTitle("New Customer");
+            } else {
+                view.add(new JLabel("ID: "));
+                JTextField idField = new JTextField(2);
+                idField.setText("" + customer.getCustomer_id());
+                idField.setEditable(false);
+                view.add(idField);
+                setTitle("Edit Customer");
+            }
+            view.add(new JLabel("First Name: "));
+            view.add(fName);
+            fName.setText(customer.getFirst_name());
+            view.add(new JLabel("Last Name: "));
+            view.add(lName);
+            lName.setText(customer.getLast_name());
+            view.add(new JLabel("Phone no: "));
+            view.add(phoneNo);
+            phoneNo.setText(customer.getPhone_no());
+            view.add(new JLabel("Address ID: "));
+            view.add(addressID);
+            addressID.setText("" + customer.getAddress().getAddress_id());
+
+            setSize(800,120);
+            setResizable(false);
+            setVisible(true);
+            setLocation(parentFrame.getLocation());
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == bUpdate) {
+                try {
+                    dao.updateCustomer(customer);
+                    this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+                } catch (DAOExceptionHandler exc) {
+                    exc.printStackTrace();
+                }
+            } else if (e.getSource() == bDelete) {
+                try {
+                    dao.deleteCustomer(customer);
+                    this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+                } catch (DAOExceptionHandler exc) {
+                    exc.printStackTrace();
+                }
+            } else if (e.getSource() == bCancel) {
+                this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+            }
+        }
+    }
+}
